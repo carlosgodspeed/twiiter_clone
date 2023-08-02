0dc489a182875e17cd87cbd37ce165a03e7aa915
carlosgodspeed <carlosminibics@gmail.com>
2023-08-02 18:21:00 -0300

    registrando usuarios

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controllers;
 
-//os recursos do miniframework
 use MF\Controller\Action;
 use MF\Model\Container;
 
@@ -14,7 +13,46 @@ class IndexController extends Action {
 	}
 
 	public function inscreverse() {
+
+		$this->view->usuario = array(
+				'nome' => '',
+				'email' => '',
+				'senha' => '',
+			);
+
+		$this->view->erroCadastro = false;
+
 		$this->render('inscreverse');
+	}
+
+	public function registrar() {
+
+		$usuario = Container::getModel('Usuario');
+
+		$usuario->__set('nome', $_POST['nome']);
+		$usuario->__set('email', $_POST['email']);
+		$usuario->__set('senha', $_POST['senha']);
+
+		
+		if($usuario->validarCadastro() && count($usuario->getUsuarioPorEmail()) == 0) {
+		
+				$usuario->salvar();
+
+				$this->render('cadastro');
+
+		} else {
+
+			$this->view->usuario = array(
+				'nome' => $_POST['nome'],
+				'email' => $_POST['email'],
+				'senha' => $_POST['senha'],
+			);
+
+			$this->view->erroCadastro = true;
+
+			$this->render('inscreverse');
+		}
+
 	}
 
 }
