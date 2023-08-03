d177bd295064311781835cccf8e3179509d9616d
carlosgodspeed <carlosminibics@gmail.com>
2023-08-02 22:34:31 -0300

    atualização na autenticação de usuarios

--- App/Models/Usuario.php
+++ App/Models/Usuario.php
@@ -61,6 +61,18 @@ class Usuario extends Model {
 
 		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
 	}
+
+	public function autenticar() {
+		$query = "select id, nome, email from usuarios where email = :email and senha = :senha";
+		$stmt = $this->db->prepare($query);
+		$stmt->bindValue(':email', $this->__get('email'));
+		$stmt->bindValue(':senha', $this->__get('senha'));
+		$stmt->execute();
+
+		$usuario = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+		return $usuario;
+	}
 }
 
 ?>
